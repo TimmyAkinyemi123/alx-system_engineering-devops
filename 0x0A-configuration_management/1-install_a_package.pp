5292b79846a52ef54b41cc2775cfe1742f50e5bf
timmyakinyemi <akinyemitimilehin044@gmail.com>
2024-05-08 00:03:39 -0700

    puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
 #Install flask from pip3
- 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-# Install Werkzeug version 2.1.1
+#Install Werkzeug version 2.1.1
 package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
